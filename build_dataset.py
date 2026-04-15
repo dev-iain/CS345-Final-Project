0f31e33e496f99c7aca58154b40d74cdba37aa31
Iain Ross <iain.ross2027@gmail.com>
2026-04-15 13:49:12 -0600

    modified dataset so it only has numeric values, not arrays

--- build_dataset.py
+++ build_dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from query import query_to_df, df_to_feature
-
-
+from pathlib import Path
+import os
 
 def build(max, endpoint, query, limit, offset):
     print("Building dataset...")
@@ -26,13 +26,30 @@ def build(max, endpoint, query, limit, offset):
     print(f"Total rows collected: {len(games_df)}")
     
     return games_df
-    
+
 def save(df, filename, **kwargs):
     df.to_csv(f'{filename}.csv', **kwargs)
     print(f"Saved to {filename}.csv")
 
+def delete(filepath):
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    else:
+        print("File does not exist.")
 
-most_reviewed_df = build(5000, 
+def clear(dir_path):
+    for filename in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+            print(f"Deleted file: {filename}")
+
+def updateAll(limit=100):
+    clear("datasets")
+    buildAll(limit)
+
+def buildAll(limit):
+    most_reviewed_df = build(limit, 
                          'popularity_primitives', 
                          """
                         fields game_id, value;
@@ -42,23 +59,52 @@ most_reviewed_df = build(5000,
                         500,
                         0
                     )
+    save(most_reviewed_df, "datasets/most_reviewed", index=False)
+    
+    review_lookup = df_to_feature(most_reviewed_df, "game_id")
+    
+    pos_popscore = build(
+        limit, 
+        "popularity_primitives",
+        f"""
+        fields game_id, value;
+        where game_id = ({review_lookup}) & popularity_type = 6;
+        """,
+        500,
+        0
+    )
+    pos_popscore = pos_popscore.rename(columns = {"value": "positive_reviews"})
 
-save(most_reviewed_df, "datasets/most_reviewed", index=False)
-review_lookup = df_to_feature(most_reviewed_df, "game_id")
+    neg_popscore = build(
+        limit,
+        "popularity_primitives",
+        f"""
+        fields game_id, value;
+        where game_id = ({review_lookup}) & popularity_type = 7;
+        """,
+        500,
+        0
+    )
+    neg_popscore = neg_popscore.rename(columns={"value": "negative_reviews"})
 
-save(build(
-            5000,             
+    games_raw_df = build(
+            limit,             
            "games",
             f"""
-            fields id, name,
-                aggregated_rating, aggregated_rating_count,
-                follows, hypes,
-                genres, platforms,
-                first_release_date;
+            fields id,
+                aggregated_rating, aggregated_rating_count, 
+                rating, rating_count,
+                follows, first_release_date;
             where id = ({review_lookup}) & platforms = [6] & aggregated_rating != null;
             """,
             500, 
             0
-           ), 
-     "datasets/games_raw", index=False)
-                        
+           )
+    games_raw_df = games_raw_df.set_index("id")
+    games_raw_df = games_raw_df.join(pos_popscore.set_index("game_id")[["positive_reviews"]])
+    games_raw_df = games_raw_df.join(neg_popscore.set_index("game_id")[["negative_reviews"]])
+    print(games_raw_df.to_string())
+    save(games_raw_df, "datasets/games_raw")
+
+updateAll(500)
+
