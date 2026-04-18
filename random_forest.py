92448ac117f79da93dc2561575723721edbc9eee
Jack Westerberg <jjwesty7@colostate.edu>
2026-04-18 11:30:56 -0600

    Added random forest

---
+++ random_forest.py
@@ -0,0 +1,36 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("datasets/games_raw.csv")
+df = df.dropna()
+
+
+X = df.drop(columns = ["aggregated_rating", "id"])
+y = df["aggregated_rating"]
+
+train_X, test_X, train_y, test_y = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+rand_forest = RandomForestRegressor(n_estimators = 100, random_state = 42)
+
+rand_forest.fit(train_X, train_y)
+pred_y = rand_forest.predict(test_X)
+
+rmse = np.sqrt(mean_squared_error(test_y, pred_y))
+
+r2 = r2_score(test_y, pred_y)
+
+print("RMSE:", rmse)
+print("R2:", r2)
+
+importances = rand_forest.feature_importances_
+feature_names = X.columns
+
+feature_df = pd.DataFrame({
+    "feature": feature_names,
+    "importance": importances
+}).sort_values(by = "importance", ascending = False)
+
+print(feature_df)
